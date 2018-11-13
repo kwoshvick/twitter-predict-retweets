46ff578a590cec6b22f112a572644820815cd17f
kwoshvick <kwoshvick@gmail.com>
2018-11-13 15:52:38 +0300

    code refactoring

--- clean.py
+++ clean.py
@@ -1,66 +1,39 @@
 import csv
-import pandas as pd
+import json
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
 
-jsonDict = dict()
-finalDict = dict()
-location = list()
-followers_count = list()
-friends_count = list()
-listed_count = list()
-favourites_count = list()
-favorite_count = list()
-sentiment_class = list()
-sentiment_pos = list()
-sentiment_neg = list()
+def csvWriter(list):
+    with open("data/sorted_csv.csv", 'a') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(list)
 
-myjson = pd.read_json('data/train.json')
-for data in myjson:
-    jsonDict[data] = list(myjson[data])
+myJsonString = open('data/practise.json')
 
+headers = ['created_at','location','followers_count','friends_count',
+           'listed_count','favourites_count','sentiment_class',
+           'sentiment_pos','sentiment_neg','retweet_count']
 
-for userData in jsonDict['user']:
-    location.append(userData['location'])
-    followers_count.append(userData['followers_count'])
-    friends_count.append(userData['friends_count'])
-    listed_count.append(userData['listed_count'])
-    favourites_count.append(userData['favourites_count'])
+jsonObjects = json.load(myJsonString)
 
-finalDict['followers_count'] = followers_count
-finalDict['friends_count'] = friends_count
-finalDict['listed_count'] = listed_count
-finalDict['favourites_count'] = favourites_count
-finalDict['favorite_count'] = favorite_count
-finalDict['favorite_count'] = jsonDict['favorite_count']
+with open("data/sorted_csv.csv", "w") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(headers)
+    outfile.close()
 
-for text in  jsonDict['text']:
+for jsonObject in jsonObjects:
+    jsonList = list()
+    jsonList.append(jsonObject['created_at'].replace("+0000 ",""))
+    jsonList.append(jsonObject['user']['location'])
+    jsonList.append(jsonObject['user']['followers_count'])
+    jsonList.append(jsonObject['user']['friends_count'])
+    jsonList.append(jsonObject['user']['listed_count'])
+    jsonList.append(jsonObject['user']['favourites_count'])
+    text = jsonObject['text']
     blob = TextBlob(text,analyzer=NaiveBayesAnalyzer())
     sentiment_values = blob.sentiment
-    sentiment_class.append(sentiment_values[0])
-    sentiment_pos.append(sentiment_values[1])
-    sentiment_neg.append(sentiment_values[2])
-
-finalDict['sentiment_class'] = sentiment_class
-finalDict['sentiment_pos'] = sentiment_pos
-finalDict['sentiment_neg'] = sentiment_neg
-
-finalDict['created_at'] = jsonDict['created_at']
-finalDict['retweet_count'] = jsonDict['retweet_count']
-
-keys = sorted(finalDict.keys())
-with open("sorted_csv.csv", "w") as outfile:
-   writer = csv.writer(outfile, delimiter = "\t")
-   writer.writerow(keys)
-   writer.writerows(zip(*[finalDict[key] for key in keys]))
-
-
-
-
-
-
-
-
-
-
-
+    jsonList.append(sentiment_values[0]) #class
+    jsonList.append(sentiment_values[1]) # pos
+    jsonList.append(sentiment_values[2]) # negative
+    jsonList.append(jsonObject['retweet_count'])
+    csvWriter(jsonList)
